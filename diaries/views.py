746ba227ee3349508947b5382e448baa955984ce
Kudoas <sakanactor624@gmail.com>
2019-03-16 16:09:20 +0900

    :sparkles:

--- diaries/views.py
+++ diaries/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Diary
+from .form import DiaryForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from django.views import generic
 
 
-def index(request):
-    return render(request, 'diaries/index.html')
+# @login_required
+class IndexView(generic.ListView):
+    model = Diary
+    template_name = 'diaries/index.html'
+    paginate_by = 3
 
 
-def detail(request):
-    return render(request, 'diaries/detail.html')
+def detail(request, diary_id):
+    diary = get_object_or_404(Diary, id=diary_id)
+    return render(request, 'diaries/detail.html', {'diary': diary})
 
 
 def create(request):
-    return render(request, 'diaries/create.html')
+    if request.method == 'POST':
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('diaries:index')
+    else:
+        form = DiaryForm
+    return render(request, 'diaries/create.html', {'form': form})
+
+
+@require_POST
+def delete(request, diary_id):
+    memo = get_object_or_404(Diary, id=diary_id)
+    memo.delete()
+    return redirect('diaries:index')
